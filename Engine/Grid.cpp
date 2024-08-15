0499cecc3972b693e7035f33680ca2176ba29828
EliasSahli <79102437+EliasSahli@users.noreply.github.com>
2024-08-15 12:38:53 +0200

    Generated random grid of black and white cells

---
+++ Engine/Grid.cpp
@@ -0,0 +1,32 @@
+#include "Grid.h"
+#include <random>
+
+void Grid::Draw(Graphics& gfx)
+{
+	for (int i = 0; i < columns; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			if (grid[i][j] < 1)
+			{
+				gfx.DrawRectDim(i * resolution, j * resolution, resolution, resolution, Colors::White);
+			}
+		}
+	}
+}
+
+void Grid::Spawn(std::mt19937& rng)
+{
+	std::uniform_int_distribution<int> dist(0, 50);
+
+	for (int i = 0; i < columns; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			grid[i][j] = dist(rng);
+		}
+	}
+}
+
+
+
---
+++ Engine/Grid.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Graphics.h"
+#include <random>
+
+
+class Grid
+{
+public:
+	void Draw(Graphics& gfx);
+	void Spawn(std::mt19937& rng);
+
+private:
+	static constexpr int resolution = 10;
+	static constexpr int columns = Graphics::ScreenWidth / resolution;
+	static constexpr int rows = Graphics::ScreenHeight / resolution;
+	int grid[columns][rows];
+};
